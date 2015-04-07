bee5a7558d69b943187505b400cb9265ff975ddf
rsteenwyk <rsteenwyk@gmail.com>
2015-04-06 21:42:00 -0400

    Parameter / begin / process block
    Added additional paramater block settings, added in begin and process
    settings.
    
    Need to figure out default param setting so it passes in "All Computer"
    correctly instead of prompting if no -filter is defined.
    
    Also, need to come up with a custom PS Object to pass to include
    relevant data.

--- PS NS/Get-Collection.ps1
+++ PS NS/Get-Collection.ps1
@@ -6,14 +6,14 @@
 #
 
 
-function global:Get-Filter {
+function global:Get-NSFilter {
 
     <#
     .SYNOPSIS
     Gets the Altiris/SMP collection (filter) based on name. Only works executing locally on SMP server.
 
     .DESCRIPTION
-    Gets filter, with ability to search by name. Allows you to get the GUID for use in other functions, like Get-FilterMembership.
+    Gets filter, with ability to search by name. Allows you to get the GUID for use in other functions, like Get-NSFilterMembership.
     #>
 
     [CmdletBinding(
@@ -21,23 +21,35 @@ function global:Get-Filter {
     ConfirmImpact='Low'
 	)]
 
-    param(
+    param
+     (
 
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)]
-        [string[]]$Filter="TestFilter"
-        )
+        [parameter(Mandatory=$True,
+        ValueFromPipeline=$True,
+        ValueFromPipelineByPropertyName=$True,
+        HelpMessage='What filter are you looking for? Remember to use an exact name.')]
+        
+        [string[]]$Filter= "All Computers"
+     
+        #[string]]]$GUID = 'eb3a1a12-e1c7-4431-b060-f0333e4e488c'
+     
+     )
     
-    
+
+    begin {
+    Write-Verbose "Attempting to connect to Altiris COM objects, using credentials scripts is running under, presumed SMP Administrators. Presumed to be on Notification Server."
+    $NSItem = New-Object -ComObject Altiris.ASDK.NS.ItemManagement
+    $NSItem.TargetServer = "localhost"
+    $NSItem.Authenticate()
+    }    
 
 
-#Calls ItemManagement class into NSItem object, sets server as local computer, and authenticates with credentials of user
-#running the script.
-$NSItem = New-Object -ComObject Altiris.ASDK.NS.ItemManagement
-$NSItem.TargetServer = "localhost"
-$NSItem.Authenticate()
+    process {
 
 $NSItem.GetItemsByName("$Filter")
 
+
+}
 
 
 }
