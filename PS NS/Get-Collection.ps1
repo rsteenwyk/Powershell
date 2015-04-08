191a0d16ce29d2ef2c4c1a8ecde80828c02818cc
rsteenwyk <rsteenwyk@gmail.com>
2015-04-07 22:56:49 -0400

    ParameterSetName fixed
    Correctly passes in a default value now.

--- PS NS/Get-Collection.ps1
+++ PS NS/Get-Collection.ps1
@@ -21,35 +21,41 @@ function global:Get-NSFilter {
     ConfirmImpact='Low'
 	)]
 
-    param
-     (
-
-        [parameter(Mandatory=$True,
-        ValueFromPipeline=$True,
-        ValueFromPipelineByPropertyName=$True,
-        HelpMessage='What filter are you looking for? Remember to use an exact name.')]
+    param (
         
-        [string[]]$Filter= "All Computers"
+        [parameter(ParameterSetName="Filter")] [String] $Filter = "All Computers"
+
+
+
+        #ParameterSetName="Filter"
+        #$Filter = 'All Computers'
+        
+        
+        
      
-        #[string]]]$GUID = 'eb3a1a12-e1c7-4431-b060-f0333e4e488c'
-     
-     )
+         )
+    
     
 
     begin {
-    Write-Verbose "Attempting to connect to Altiris COM objects, using credentials scripts is running under, presumed SMP Administrators. Presumed to be on Notification Server."
-    $NSItem = New-Object -ComObject Altiris.ASDK.NS.ItemManagement
-    $NSItem.TargetServer = "localhost"
-    $NSItem.Authenticate()
+Write-Verbose "Attempting to connect to Altiris COM objects, using credentials scripts is running under, presumed SMP Administrators. Presumed to be on Notification Server."
+$NSItem = New-Object -ComObject Altiris.ASDK.NS.ItemManagement
+$NSItem.TargetServer = "localhost"
+$NSItem.Authenticate()
     }    
 
 
     process {
-
+Foreach ($FilterName in $Filter)
+{
 $NSItem.GetItemsByName("$Filter")
-
+}
 
 }
 
 
 }
+
+
+#[string]]]$GUID = 'eb3a1a12-e1c7-4431-b060-f0333e4e488c'
+#(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,)
