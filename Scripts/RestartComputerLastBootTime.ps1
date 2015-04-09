b96d9ffa47e5815871e07fa1668f1e8891392fbd
rsteenwyk <rsteenwyk@gmail.com>
2015-04-09 19:13:18 -0400

    Quick and dirty script

---
+++ Scripts/RestartComputerLastBootTime.ps1
@@ -0,0 +1,15 @@
+﻿$Boottime = Get-WmiObject win32_operatingsystem | select @{LABEL='LastBootUpTime'
+
+;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+
+
+$Acceptable = Get-Date -Date 2015-04-09 -Hour 20 -Minute 10
+
+if ($Boottime.LastBootUpTime -lt $Acceptable)
+{
+shutdown.exe /r /t 90
+}
+else
+{
+Exit
+}
